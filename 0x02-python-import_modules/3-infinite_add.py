8745d7b5e6efc1660d434e490da25afad0568f3b
codenvibes <terrymk1999@gmail.com>
2023-08-12 16:43:38 +0300

    Create a program that prints the sum of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,9 @@ File: 3-infinite_add.py
 """
 import sys
 
-if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    result = 0
+argc = len(sys.argv) - 1
+result = 0
 
-    for i in range(1, argc + 1):
-        result += int(sys.argv[i])
-    print("result =", result)
+for i in range(1, argc + 1):
+    result += int(sys.argv[i])
+print(result)
