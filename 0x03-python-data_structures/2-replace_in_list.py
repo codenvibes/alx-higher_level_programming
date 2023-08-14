4df6bcbda7c2f407d56db0f00d41149c37256a8f
codenvibes <terrymk1999@gmail.com>
2023-08-15 02:07:02 +0300

    Create a function that replaces an element of a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@
 # A function that replaces an element of a list
 
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 or idx < (len(my_list)):
+    if idx >= 0 and idx < (len(my_list)):
         my_list[idx] = element
     return (my_list)
