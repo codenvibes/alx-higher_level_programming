8267fe23842f19c85ee3345b705ad9760ba622bf
codenvibes <terrymk1999@gmail.com>
2023-08-18 01:54:26 +0300

    Create a function that returns elements that are common to both sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author: Terrence M.K
+# File: 3-common_elements.py
+def common_elements(set_1, set_2):
+    """Returns elements that are common to both sets"""
+    common = set_1.intersection(set_2)
+    return (common)
