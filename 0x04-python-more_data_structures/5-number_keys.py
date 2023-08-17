916aee7881f8b80b3227a860c8c74da48287e8e7
codenvibes <terrymk1999@gmail.com>
2023-08-18 02:09:29 +0300

    Create a function that returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author: Terrence M.K
+# File: 5-number_keys.py
+def number_keys(a_dictionary):
+    """returns the number of keys in a dictionary"""
+    return (len(a_dictionary))
