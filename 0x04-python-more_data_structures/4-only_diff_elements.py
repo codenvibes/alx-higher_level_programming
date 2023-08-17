56c0fd427de03508e45a5db9b4449fdedcc27f21
codenvibes <terrymk1999@gmail.com>
2023-08-18 02:02:58 +0300

    Create a function that returns the symmetric difference between two sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author: Terrence M.K
+# File: 4-only_diff_elements.py
+def only_diff_elements(set_1, set_2):
+    """returns the symmetric difference between two sets"""
+    difference = set_1.symmetric_difference(set_2)
+    return (difference)
