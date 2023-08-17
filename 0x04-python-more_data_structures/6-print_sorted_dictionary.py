174859b5cb5b61939fc3ffd6b4e73dc0f2815e05
codenvibes <terrymk1999@gmail.com>
2023-08-18 02:39:10 +0300

    Create a function that prints a sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,4 +6,4 @@ def print_sorted_dictionary(a_dictionary):
     sorted_items = sorted(a_dictionary.items())
 
     for key, value in sorted_items:
-        print(key, ":", value)
+        print("{:s} : {}".format(key, value))
