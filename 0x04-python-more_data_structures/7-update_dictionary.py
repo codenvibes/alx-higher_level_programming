fb7efefd368a74eda14ac33ed25824013117876f
codenvibes <terrymk1999@gmail.com>
2023-08-18 02:55:53 +0300

    Create a function that adds a key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,6 @@
 # Author: Terrence M.K
 # File: 7-update_dictionary.py
 def update_dictionary(a_dictionary, key, value):
+    """add a key/value in a dictionary"""
     a_dictionary[key] = value
     return (a_dictionary)
