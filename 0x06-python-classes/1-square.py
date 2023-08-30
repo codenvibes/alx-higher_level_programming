8ce97064b2fa3f8e612b6d5b1319899f7e7d94ab
codenvibes <terrymk1999@gmail.com>
2023-08-30 23:55:45 +0300

    Create a class Square...
    with aprivate instance attribute: size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 # Auth: Terrence M.K
 # File: 1-square.py
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
