3da16b6969d8de1939b20b7f341c88fc9a287b7d
codenvibes <terrymk1999@gmail.com>
2023-09-07 00:31:54 +0300

    Create a program that solves the N queens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Auth: Terrence M.K
+# File: 101-nqueens.py
+"""This function solves the N-queens puzzle by finding all possible solutions for placing N non-attacking queens on an NxN chessboard.
+
+Usage:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
