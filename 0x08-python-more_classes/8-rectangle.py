71035777f2561a0c004075c5c5b9d07acbb32c79
codenvibes <terrymk1999@gmail.com>
2023-09-07 00:16:35 +0300

    Add Task 8 test module

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Auth: Terrence M.K
+# File: 8-rectangle.py
+"""This module defines a simple Rectangle class."""
+
+
+class Rectangle:
+    """This class represents a rectangle."""
