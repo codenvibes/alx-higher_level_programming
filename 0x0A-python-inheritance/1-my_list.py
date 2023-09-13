7f0eb6a3487f365b75b65a8b3b4c0596f5548f0e
codenvibes <terrymk1999@gmail.com>
2023-09-13 18:49:52 +0300

    Create a class MyList that inherits from list and...
    a Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,14 +9,9 @@ This module defines a custom list class.
 class MyList(list):
     """
     A custom list class that inherites from the built-in list class.
-
-    This class adds a method to print the elements of the list in sorted
-    order.
     """
     def print_sorted(self):
         """
-        Print the elements of the list in sorted order.
-
         This method creates a copy of the list, sorts the copy, and then
         prints it.
         """
