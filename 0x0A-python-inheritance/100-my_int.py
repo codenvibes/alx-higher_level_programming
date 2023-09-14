fdecf02877ef48f578ed1fc60400205d67f7cbdf
codenvibes <terrymk1999@gmail.com>
2023-09-14 03:36:12 +0300

    Create  a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -14,25 +14,11 @@ class MyInt(int):
     def __eq__(self, value):
         """
         Custom equality comparison (__eq__) method.
-
-        Args:
-            value (int): The value to compare with.
-
-        Returns:
-            bool: True if the real part of this MyInt is not equal to
-            the given value, False otherwise.
         """
         return self.real != value
 
     def __ne__(self, value):
         """
         Custom inequality comparison (__ne__) method.
-
-        Args:
-            value (int): The value to compare with.
-
-        Returns:
-            bool: True if the real part of this MyInt is equal to the given
-            value, False otherwise.
         """
         return self.real == value
