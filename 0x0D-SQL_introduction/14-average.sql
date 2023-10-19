eb296efe1aa73e46c277fc57c9ab930e44430d6e
codenvibes <terrymk1999@gmail.com>
2023-10-19 14:33:18 +0300

    Create a MYSQL script that computes the score average of all records in 'second_table'

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- A MYSQL script that computes the score average of all records in 'second_table' 
-SELECT AVG(score) AS average_score FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
