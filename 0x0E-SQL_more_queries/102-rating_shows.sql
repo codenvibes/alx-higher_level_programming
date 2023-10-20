545a26027588041dba17e7ab729b65ad05af6eeb
codenvibes <terrymk1999@gmail.com>
2023-10-20 03:43:15 +0300

    Create a  MYSQL script that lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,8 @@
 -- A MYSQL script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
+SELECT title, SUM(rate) AS rating
+  FROM tv_shows AS t
+       INNER JOIN tv_show_ratings AS r
+       ON t.id = r.show_id
+ GROUP BY title
+ ORDER BY rating DESC;
