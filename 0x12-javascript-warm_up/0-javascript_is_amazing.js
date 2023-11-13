b6fc430a2ec77491bdc3466849bbc702bd74fdc7
codenvibes <terrymk1999@gmail.com>
2023-11-14 00:20:38 +0300

    Create a JS script that prints “JavaScript is amazing”

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
+/*
+    Auth: codenvibes
+    Desc: A JS script that prints “JavaScript is amazing”.
+*/
 const myVar = "JavaScript is amazing";
-console.log(myVar)
+console.log(myVar);
