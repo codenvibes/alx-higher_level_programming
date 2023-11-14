87dd9ea7f21c557006f4d37bd59a9c37b71e7452
codenvibes <terrymk1999@gmail.com>
2023-11-14 11:28:02 +0300

    Create a JS script that prints the 3 specified lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+    Auth: codenvibes
+    Desc: A JS script that prints 3 lines:
+        - The first line: “C is fun”
+        - The second line: “Python is cool”
+        - The third line: “JavaScript is amazing”
+*/
+console.log('C is fun\nPython is cool\nJavaScript is amazing')
