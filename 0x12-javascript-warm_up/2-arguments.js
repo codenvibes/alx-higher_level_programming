f1d5d516301b0a517ba99dec7fc40373f696462c
codenvibes <terrymk1999@gmail.com>
2023-11-16 02:00:26 +0300

    Create a JS script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-/*
-    Auth: codenvibes
-    Desc: A JS script that prints a message depending of the number of arguments passed:
-        - If no arguments are passed to the script, print “No argument”
-        - If only one argument is passed to the script, print “Argument found”
-        - Otherwise, print “Arguments found”
-*/
-const cmdArgs = process.argv.length;
-if (cmdArgs === 2) {
-    console.log('No argument');
-} else if (cmdArgs === 3) {
-    console.log('Argument found');
+
+const argumentsCount = process.argv.length - 2;
+
+if (argumentsCount === 0) {
+  console.log('No argument');
+} else if (argumentsCount === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
