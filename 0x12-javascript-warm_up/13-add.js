9b5611905dd067aebb810105eaccde815817d56e
codenvibes <terrymk1999@gmail.com>
2023-11-16 04:19:17 +0300

    Create a JS function that returns the addition of 2 integers

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+    Auth: codenvibes
+    Desc: JS script - a function that returns the addition of 2 integers
+*/
+function add (a, b) {
+  return a + b;
+}
+exports.add = add;
