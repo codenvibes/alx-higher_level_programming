510685e152da5891fd8c99d7bec413a66dc81e1f
codenvibes <terrymk1999@gmail.com>
2023-11-16 04:00:44 +0300

    Create a JS script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+    Auth: codenvibes
+    Desc: JS script - Prints the addition of 2 integers
+*/
+const process = require('process');
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(firstArg, secondArg));
