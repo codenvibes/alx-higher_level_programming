d8188734d8a8b7c431a33222037fd6c0e05b6428
codenvibes <terrymk1999@gmail.com>
2023-11-16 03:53:52 +0300

    Create a JS script that prints 'C is fun' x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+    Auth: codenvibes
+    Desc: JS script - Prints “C is fun” x times
+*/
+const process = require('process');
+const firstArg = process.argv[2];
+
+const x = parseInt(firstArg);
+
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
