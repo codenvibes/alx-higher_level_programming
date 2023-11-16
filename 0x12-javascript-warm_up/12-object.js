7d96d6b24b7f16600a378c8ea3e9b7b4cebe2c27
codenvibes <terrymk1999@gmail.com>
2023-11-16 04:09:13 +0300

    Update the given JS script to replace the value 12 with 89

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -4,9 +4,9 @@
     Desc: Update the given JS script to replace the value 12 with 89
 */
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
