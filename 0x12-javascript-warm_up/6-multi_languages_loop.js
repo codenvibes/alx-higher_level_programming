1729e272f3eb23bb339adda944a8ef1ea934828b
codenvibes <terrymk1999@gmail.com>
2023-11-16 03:46:00 +0300

    Create a JS script that prints 3 lines: (like 1-multi_languages.js) but by using an array and loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -10,5 +10,5 @@
 */
 languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 for (let index = 0; index < languages.length; index++) {
-    console.log(languages[index])
+  console.log(languages[index])
 }
