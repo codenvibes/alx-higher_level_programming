f00b47fc6f2ce6eec13b24da8cdd14a33f2ec56e
codenvibes <terrymk1999@gmail.com>
2023-11-21 00:14:29 +0300

    Create a python script that takes in the name of a state as an argument and lists all cities of that state
    ...using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,12 +11,13 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = "SELECT cities.name, states.name FROM cities JOIN states ON\
-        cities.state_id = states.id WHERE states.name LIKE BINARY %s;"
+    query = "SELECT cities.name FROM cities JOIN states ON\
+        cities.state_id = states.id WHERE states.name LIKE BINARY %s\
+        ORDER BY cities.id;"
     state_name = sys.argv[4]
     cursor.execute(query, (state_name,))
     cities_in_state = cursor.fetchall()
-    for city in cities_in_state:
-        print(city)
+    city_names = ', '.join(city[0] for city in cities_in_state)
+    print(city_names)
     cursor.close()
     db.close()
