48ea39678d7bf83af0d7462486e496a6bf4b328e
codenvibes <terrymk1999@gmail.com>
2023-11-20 18:57:03 +0300

    Create a python script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,8 +10,8 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states\
-        ON cities.state_id = states.id;"
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN\
+        states ON cities.state_id = states.id;"
     cursor.execute(query)
     states = cursor.fetchall()
     for match in states:
