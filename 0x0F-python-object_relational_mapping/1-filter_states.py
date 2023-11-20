af447535bac74497f7c455da197c85a42e959c45
codenvibes <terrymk1999@gmail.com>
2023-11-20 13:38:43 +0300

    Create a python script that lists all states with a name starting with N from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,13 @@ import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
-# stwn - states starting with N
-stwn = cursor.fetchall()
-for state in stwn:
-    print(state)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    # stwn - states starting with N
+    stwn = cursor.fetchall()
+    for state in stwn:
+        print(state)
+    cursor.close()
+    db.close()
