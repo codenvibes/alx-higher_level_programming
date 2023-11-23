5b3e66c04127847e59267a5443d4882f3dd01da6
codenvibes <terrymk1999@gmail.com>
2023-11-23 15:43:49 +0300

    Initialize Database Tables
    Create SQLAlchemy engine to connect to a MySQL database and generate tables based on the data model defined in the model_state module. Connection pre-pinging is enabled for improved connection management

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -7,5 +7,5 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True) # https://github.com/codenvibes/alx-higher_level_programming/blob/master/0x0F-python-object_relational_mapping/6-model_state.md
+    Base.metadata.create_all(engine) # This line creates the tables defined in the SQLAlchemy model. The Base.metadata.create_all(engine) call instructs SQLAlchemy to create the database tables associated with the classes defined in the Base using the provided database engine.
