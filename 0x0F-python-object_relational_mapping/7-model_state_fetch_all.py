c48567ffa4ff1ffd9e52b80a8921a9bc0849bed8
codenvibes <terrymk1999@gmail.com>
2023-11-23 20:14:38 +0300

    Create a python script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,13 +4,14 @@
 This module lists all State objects from the database hbtn_0e_6_usa.
 """
 import sys
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(username, password, database_name),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
