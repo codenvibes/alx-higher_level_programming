74ae453ec05de79e3a339a128f6f31c8f88ded1c
codenvibes <terrymk1999@gmail.com>
2023-12-05 14:15:31 +0300

    Chore: Create a Bash script for POST request with specified variables (email, subject)

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # a Bash script for POST request with specified variables (email, subject)
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
