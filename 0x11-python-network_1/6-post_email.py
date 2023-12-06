143aafc5416e03367bd8d3211087fa2b26c80eae
codenvibes <terrymk1999@gmail.com>
2023-12-06 03:11:42 +0300

    Chore: Create a Python script that sends a POST request to a URL with an email parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# AUTH: codenvibes
+"""
+A script that sends a POST request to a specified URL with an email parameter.
+
+Parameters:
+    url (str): The target URL to which the POST request will be sent.
+    email (str): The email parameter to include in the POST request.
+Usage:
+    ./6-post_email.py <url> <email>
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    response = requests.post(url, data=data)
+
+    print("Your email is:", email)
